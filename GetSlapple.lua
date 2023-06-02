515bf19cd33b9def9c97021c79122058ba839c88
45nadobrath <135212846+45nadobrath@users.noreply.github.com>
2023-06-02 15:48:20 +0300

    Create GetSlapple.lua

---
+++ GetSlapple.lua
@@ -0,0 +1,12 @@
+local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/45nadobrath/Database/main/Database.lua", true))()
+
+if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
+ wait(0.2)
+for i,v in pairs(game:GetDescendants()) do
+if v.Name == 'Glove' then
+v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+end
+end
+else
+  game:service('Players').LocalPlayer:Kick('Bruh')
+end
