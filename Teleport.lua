d09afd4b9661c6885533a3fca95486fa3ac656cf
45nadobrath <135212846+45nadobrath@users.noreply.github.com>
2023-06-03 00:51:50 +0300

    Update Teleport.lua

--- Teleport.lua
+++ Teleport.lua
@@ -1,7 +1,7 @@
 local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/45nadobrath/Database/main/Database.lua", true))()
 
 if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
- wait(0.1)
+ wait(0.5)
 workspace.Lobby.Teleport2.CanCollide = false
 for i,v in pairs(game:GetDescendants()) do
 if v.Name == 'Teleport2' then
