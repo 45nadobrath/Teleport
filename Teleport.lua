e525c9ff97d6505879bb1b161567b7da1206711f
45nadobrath <135212846+45nadobrath@users.noreply.github.com>
2023-06-02 15:38:50 +0300

    Create Teleport.lua

---
+++ Teleport.lua
@@ -0,0 +1,13 @@
+local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/45nadobrath/Database/main/Database.lua", true))()
+
+if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
+ wait(0.1)
+workspace.Lobby.Teleport2.CanCollide = false
+for i,v in pairs(game:GetDescendants()) do
+if v.Name == 'Teleport2' then
+v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+end
+end
+else
+  game:service('Players').LocalPlayer:Kick('Bruh')
+end
